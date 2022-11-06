4887fca388ed1109223f732eb8099d165e24d95c
Yoyo <90376868+yoyo9747@users.noreply.github.com>
2022-11-06 15:57:48 +0900

    network
    network

---
+++ Assets/Scripts/Network/NetworkController.cs
@@ -0,0 +1,23 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Now connected to " + PhotonNetwork.CloudRegion + "server");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
