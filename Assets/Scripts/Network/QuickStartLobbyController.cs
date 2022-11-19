ecfc61a51f5f6b5f4478a2fbcac7ea0e5ce799e6
Yoyo <90376868+yoyo9747@users.noreply.github.com>
2022-11-19 18:40:59 +0900

    넽웤

--- Assets/Scripts/Network/QuickStartLobbyController.cs
+++ Assets/Scripts/Network/QuickStartLobbyController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class QuickStartLobbyController : MonoBehaviourPunCallbacks
 {
@@ -16,9 +17,10 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     private int RoomSize;
     
     [SerializeField]
-    private int multiplayerSceneIndex;
+    private int firstSceneIndex;
 
     public TextMeshProUGUI Text;
+    
 
     public override void OnConnectedToMaster()
     {
@@ -33,29 +35,23 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
         Debug.Log("Quick start");
         quickStartButton.GetComponent<Button>().interactable = false;
         quickCancelButton.GetComponent<Button>().interactable = true;
-        PhotonNetwork.JoinRandomRoom();
-    }
-
-    public override void OnJoinRandomFailed(short returnCode, string message)
-    {
-        Debug.Log("Failed to join a room");
-        CreateRoom();
-    }
-
-    void CreateRoom()
-    {
-        Debug.Log("Creating room now");
-        int randomRoomNumber = Random.Range(0,10000);
+        Debug.Log(Text.text);
+        Debug.Log("Create or Join room now");
         RoomOptions roomOps = new RoomOptions() {IsVisible = true, IsOpen=true, MaxPlayers=(byte)RoomSize};
-
-        PhotonNetwork.CreateRoom("Room" + randomRoomNumber, roomOps);
-        Debug.Log(randomRoomNumber);
+        roomOps.CustomRoomProperties = new Hashtable(){{"roomCode",Text.text}};
+        PhotonNetwork.JoinOrCreateRoom("Room" +"1" + Text.text, roomOps,null);
     }
 
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join room... trying again");
+        PhotonNetwork.JoinRoom("Room" +"1" + Text.text);
+    }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
+        RoomOptions roomOps = new RoomOptions() {IsVisible = true, IsOpen=true, MaxPlayers=(byte)RoomSize};
         Debug.Log("Failed to create room... trying again");
-        CreateRoom();
+        PhotonNetwork.CreateRoom("Room" +"1" + Text.text, roomOps,null);
     }
 
     public void QuickCancel()
@@ -63,9 +59,10 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
         quickStartButton.GetComponent<Button>().interactable = true;
         quickCancelButton.GetComponent<Button>().interactable = false;
         PhotonNetwork.LeaveRoom();
+        ///그냥 exit program not leave room
     }
 
-    public override void OnEnable()
+    /*public override void OnEnable()
     {
         PhotonNetwork.AddCallbackTarget(this);
     }
@@ -73,20 +70,15 @@ public class QuickStartLobbyController : MonoBehaviourPunCallbacks
     public override void OnDisable()
     {
         PhotonNetwork.RemoveCallbackTarget(this);
-    }
+    }*/
 
     public override void OnJoinedRoom()
     {
-        Debug.Log("Joined Room");
-        StartGame();
-    }
-
-    private void StartGame()
-    {
+        Debug.Log("Joined <Room" +"1" + Text.text+">");
         if(PhotonNetwork.IsMasterClient)
         {
             Debug.Log("Starting Game");
-            PhotonNetwork.LoadLevel(multiplayerSceneIndex);
+            PhotonNetwork.LoadLevel(firstSceneIndex);
         }
     }
 }
--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,28 +4,28 @@ using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.SceneManagement;
-
-public class Portal : MonoBehaviour
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+public class Portal : MonoBehaviourPunCallbacks
 {
     public int levelIndex;
     
     [SerializeField]
     private int RoomSize = 10;
-
+    private string roomCode;
+    private Hashtable hashTable;
     private void OnTriggerEnter(Collider other) {
+        hashTable = PhotonNetwork.CurrentRoom.CustomProperties;
         if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             if(other.GetComponent<PhotonView>()?.IsMine == true)
             {
                 Debug.Log("Portal Entered + " + levelIndex);
                 //PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer);
-                // PhotonNetwork.LeaveRoom();
                 PhotonNetwork.Destroy(other.gameObject);
-                
-                Debug.Log("Creating room now");
-                PhotonNetwork.AutomaticallySyncScene = false;
-                PhotonNetwork.LoadLevel(levelIndex);
-
+                roomCode=(string)hashTable["roomCode"];
+                Debug.Log("Leave room now");
+                PhotonNetwork.LeaveRoom();
+                //PhotonNetwork.AutomaticallySyncScene = false;
                 // SceneManager.LoadScene(levelIndex);
                 // RoomOptions roomOps = new RoomOptions() {IsVisible = true, IsOpen=true, MaxPlayers=(byte)RoomSize};
 
@@ -37,5 +37,33 @@ public class Portal : MonoBehaviour
 
     private void OnTriggerExit(Collider other) {
         Debug.Log("Portal Leaved");
+    }
+
+    public override void OnLeftRoom(){
+
+        RoomOptions roomOps = new RoomOptions() {IsVisible = true, IsOpen=true, MaxPlayers=(byte)RoomSize};
+        roomOps.CustomRoomProperties = new Hashtable(){{"roomCode",roomCode}};
+
+        PhotonNetwork.JoinOrCreateRoom("Room" +levelIndex + roomCode, roomOps,null);
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join room... trying again");
+        PhotonNetwork.JoinRoom("Room" +"1" + roomCode);
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        RoomOptions roomOps = new RoomOptions() {IsVisible = true, IsOpen=true, MaxPlayers=(byte)RoomSize};
+        Debug.Log("Failed to create room... trying again");
+        PhotonNetwork.CreateRoom("Room" +"1" + roomCode, roomOps,null);
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined <Room" +levelIndex + roomCode+">");
+        
+        if(PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel(levelIndex);
+        }
     }
 }
